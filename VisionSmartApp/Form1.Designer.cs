6aa46a81048458dc8ba5c15c4c95f156428598c6
Luis Enrique Mezas Aguilar <luismezas.aguilar@gmail.com>
2018-04-14 23:39:40 -0500

    added EF & emguCV packages

---
+++ VisionSmartApp/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace VisionSmartApp
+{
+    partial class FormVideo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtPersonasDetectadas = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(800, 600);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPersonasDetectadas);
+            this.groupBox1.Location = new System.Drawing.Point(818, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(186, 318);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos recabados";
+            // 
+            // txtPersonasDetectadas
+            // 
+            this.txtPersonasDetectadas.Location = new System.Drawing.Point(80, 22);
+            this.txtPersonasDetectadas.Name = "txtPersonasDetectadas";
+            this.txtPersonasDetectadas.ReadOnly = true;
+            this.txtPersonasDetectadas.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonasDetectadas.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Personas:";
+            // 
+            // FormVideo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 623);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.imageBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormVideo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Visión Artificial";
+            this.Load += new System.EventHandler(this.FormVideo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPersonasDetectadas;
+    }
+}
+
---
+++ VisionSmartApp/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace VisionSmartApp
+{
+    public partial class FormVideo : Form
+    {
+        Image<Bgr, Byte> imgOrg; //image type RGB (or Bgr as we say in Open CV)
+        private Capture capturecam;
+        public static int Counter = 0;
+
+        public FormVideo()
+        {
+            InitializeComponent();
+        }
+
+        private void FormVideo_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                capturecam = new Capture();
+            }
+            catch (NullReferenceException exception)
+            {
+                MessageBox.Show(exception.Message);
+                return;
+            }
+            Application.Idle += new EventHandler(ProcessFunction);
+        }
+
+        private void ProcessFunction(object sender, EventArgs arg)
+        {
+            imgOrg = capturecam.QueryFrame().ToImage<Bgr, Byte>(); // error line
+            FindPedestrian.Find(imgOrg);
+            imageBox1.Image = imgOrg;
+            txtPersonasDetectadas.Text = Counter.ToString();
+        }
+    }
+    public static class FindPedestrian
+    {
+        /// <summary>
+        /// Find the pedestrian in the image
+        /// </summary>
+        /// <param name="image">The image</param>
+        /// <param name="processingTime">The pedestrian detection time in milliseconds</param>
+        /// <returns>The image with pedestrian highlighted.</returns>
+        public static Image<Bgr, Byte> Find(Image<Bgr, Byte> image)
+        {
+            Rectangle[] regions = new Rectangle[5];
+            //this is the CPU version
+            using (HOGDescriptor des = new HOGDescriptor())
+            {
+                des.SetSVMDetector(HOGDescriptor.GetDefaultPeopleDetector());
+
+                Emgu.CV.Structure.MCvObjectDetection[] objects = des.DetectMultiScale(image);
+                
+                for(int i = 0; i < objects.Length; i++)
+                {
+                    regions[i] = objects[i].Rect;
+                    if (objects[i].Score > 0.50)
+                        FormVideo.Counter++;
+                }
+            }
+            foreach (Rectangle pedestrain in regions)
+            {
+                image.Draw(pedestrain, new Bgr(Color.Red), 1);
+            }
+            return image;
+        }
+    }
+}
